4b03e3b762be7df0a76b703a99e0c3a26441dea2
Sulecs1 <suleakcay96@gmail.com>
2021-03-10 01:14:32 +0300

    değişken açıklamaları eklendi

---
+++ k-means.py
@@ -0,0 +1,13 @@
+############################################################
+#   Customer Segmentation with K-Means Algorithm           #
+############################################################
+
+
+#InvoiceNo: Fatura numarası. Her işleme yani faturaya ait eşsiz numara. Eğer bu kod C ile başlıyorsa işlemin iptal edildiğini ifade eder.
+#StockCode: Ürün kodu. Her bir ürün için eşsiz numara.
+#Description: Ürün ismi
+#Quantity: Ürün adedi. Faturalardaki ürünlerden kaçar tane satıldığını ifade etmektedir.
+#InvoiceDate: Fatura tarihi ve zamanı.
+#UnitPrice: Ürün fiyatı (Sterlin cinsinden)
+#CustomerID: Eşsiz müşteri numarası
+#Country: Ülke ismi. Müşterinin yaşadığı ülke.
